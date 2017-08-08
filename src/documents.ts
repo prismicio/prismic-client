e0b46488c14904bcc4a6fc611091bdfdf217216d
Sebastien <srenault.contact@gmail.com>
2017-08-08 20:02:44 +0200

    Keep `firstPublicationDate` and `lastPublicationDate` as string

--- src/documents.ts
+++ src/documents.ts
@@ -1,4 +1,4 @@
-export class Document {
+export interface Document {
   id: string;
   uid ?: string;
   type: string;
@@ -11,33 +11,4 @@ export class Document {
   firstPublicationDate: Date | null;
   lastPublicationDate: Date | null;
   data: any;
-
-  constructor(
-    id: string,
-    type: string,
-    href: string,
-    tags: string[],
-    slug: string,
-    slugs: string[],
-    alternateLanguages: string[],
-    firstPublicationDate: string | null,
-    lastPublicationDate: string | null,
-    data: any,
-    uid ?: string,
-    lang ?: string
-  ){
-    
-    this.id = id;
-    this.uid = uid;
-    this.type = type;
-    this.href = href;
-    this.tags = tags;
-    this.slug = slug[0];
-    this.slugs = slugs;
-    this.lang = lang;
-    this.alternateLanguages = alternateLanguages;
-    this.firstPublicationDate = firstPublicationDate ? new Date(firstPublicationDate) : null;
-    this.lastPublicationDate = lastPublicationDate ? new Date(lastPublicationDate) : null,
-    this.data = data;
-  };
 }
