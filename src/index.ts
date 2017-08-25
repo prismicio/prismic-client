f3649f2f650f1ac865f48cb8f9d0745d03a767c0
Sebastien <srenault.contact@gmail.com>
2017-08-25 08:43:51 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -5,43 +5,43 @@ import { Experiments } from './experiments';
 import {
   ApiOptions,
   Api,
-  ExperimentCookie,
-  PreviewCookie,
 } from "./api";
 
-function getApi(url: string, options: ApiOptions | null): Promise<Api> {
-  const safeOptions = options || {} as ApiOptions;
-  var api = new Api(url, safeOptions);
-  //Use cached api data if available
-  return new Promise(function(resolve, reject) {
-    var cb = function(err: Error | null, api?: any) {
-      if (safeOptions.complete) safeOptions.complete(err, api);
-      if (err) {
-        reject(err);
-      } else {
-        resolve(api);
-      }
-    };
-    api.get(function (err: Error, data: any) {
-      if (!err && data) {
-        api.data = data;
-        api.bookmarks = data.bookmarks;
-        api.experiments = new Experiments(data.experiments);
-      }
+import { EXPERIMENT_COOKIE, PREVIEW_COOKIE } from './ResolvedApi';
 
-      cb(err, api);
-    });
+// function getApi(url: string, options: ApiOptions | null): Promise<Api> {
+//   const safeOptions = options || {} as ApiOptions;
+//   var api = new Api(url, safeOptions);
+//   //Use cached api data if available
+//   return new Promise(function(resolve, reject) {
+//     var cb = function(err: Error | null, api?: any) {
+//       if (safeOptions.complete) safeOptions.complete(err, api);
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(api);
+//       }
+//     };
+//     api.get(function (err: Error, data: any) {
+//       if (!err && data) {
+//         api.data = data;
+//         api.bookmarks = data.bookmarks;
+//         api.experiments = new Experiments(data.experiments);
+//       }
 
-    return api;
-  });
-}
+//       cb(err, api);
+//     });
+
+//     return api;
+//   });
+// }
 
 module.exports = {
-  experimentCookie: ExperimentCookie,
-  previewCookie: PreviewCookie,
+  experimentCookie: EXPERIMENT_COOKIE,
+  previewCookie: PREVIEW_COOKIE,
   Predicates,
   Experiments,
-  api: getApi,
+//  api: getApi,
   Api,
-  getApi
+//  getApi
 };
--- src/request.ts
+++ src/request.ts
@@ -1,69 +1,58 @@
-export interface RequestError extends Error {
-  status: number;
-}
-
-function createError(status: number, message: string): RequestError {
-  return {
-    name: "prismic-request-error",
-    message,
-    status
-  }
-}
-
 // Number of maximum simultaneous connections to the prismic server
-let MAX_CONNECTIONS: number = 20;
+const MAX_CONNECTIONS: number = 20;
 // Number of requests currently running (capped by MAX_CONNECTIONS)
 let running: number = 0;
 // Requests in queue
-let queue: any[]  = [];
+const queue: any[]  = [];
 
 interface RequestCallback {}
 
 interface RequestCallbackSuccess extends RequestCallback {
   result: any;
   xhr: any;
-  ttl ?: number;
+  ttl?: number;
 }
 
 interface RequestCallbackFailure extends RequestCallback {
-  error: Error
+  error: Error;
 }
-
 
 function fetchRequest(
   url: string,
   onSuccess: (_: RequestCallbackSuccess) => void,
-  onError: (_: RequestCallbackFailure) => void
+  onError: (_: RequestCallbackFailure) => void,
 ): any {
   return fetch(url, {
     headers: {
-      'Accept': 'application/json'
-    }
-  }).then(function (response) {
-    if (~~(response.status / 100 != 2)) {
-      throw createError(response.status, "Unexpected status code [" + response.status + "] on URL " + url);
+      Accept: 'application/json',
+    },
+  }).then((response) => {
+    if (~~(response.status / 100 !== 2)) {
+      const e = new Error(`Unexpected status code [${response.status}] on URL ${url}`);
+      e.status = response.status;
+      throw e;
     } else {
-      return response.json().then(function(json) {
+      return response.json().then((json) => {
         return {
-          response: response,
-          json: json
+          response,
+          json,
         };
       });
     }
-  }).then(function(next: any) {
-    var response = next.response;
-    var json = next.json;
-    var cacheControl = response.headers.get('cache-control');
+  }).then((next: any) => {
+    const response = next.response;
+    const json = next.json;
+    const cacheControl = response.headers.get('cache-control');
     const parsedCacheControl: string[] | null = cacheControl ? /max-age=(\d+)/.exec(cacheControl) : null;
     const ttl = parsedCacheControl ? parseInt(parsedCacheControl[1], 10) : undefined;
-    onSuccess({result: json, xhr: response, ttl} as RequestCallbackSuccess);
-  }).catch(function (error: Error) {
-    onError({error} as RequestCallbackFailure);
+    onSuccess({ ttl, result: json, xhr: response } as RequestCallbackSuccess);
+  }).catch((error: Error) => {
+    onError({ error } as RequestCallbackFailure);
   });
 }
 
 export interface RequestHandler {
-  request(url: String, cb: (error: Error | null, result?: any, xhr?: any) => void): void
+  request(url: String, cb: (error: Error | null, result?: any, xhr?: any) => void): void;
 }
 
 function processQueue() {
@@ -74,24 +63,25 @@ function processQueue() {
 
   const next = queue.shift();
 
-  fetchRequest(next.url,
-    function({result, xhr, ttl}: RequestCallbackSuccess) {
+  const onSuccess = ({ result, xhr, ttl }: RequestCallbackSuccess) => {
       running--;
       next.callback(null, result, xhr, ttl);
       processQueue();
-    },
-    function({error}: RequestCallbackFailure) {
-      next.callback(error);
-      processQueue();
-    }
-  );
+  };
+
+  const onError = ({ error }: RequestCallbackFailure) => {
+    next.callback(error);
+    processQueue();
+  };
+
+  fetchRequest(next.url, onSuccess, onError);
 }
 
 export class DefaultRequestHandler implements RequestHandler {
   request(url: String, cb: (error: Error | null, result?: any, xhr?: any, ttl?: number) => void): void {
     queue.push({
-      'url': url,
-      'callback': cb
+      url,
+      callback: cb,
     });
     processQueue();
   }
