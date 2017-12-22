3dc7cb3971a7c2eb2ad40ee16ad37d84825eff91
Sebastien <srenault.contact@gmail.com>
2017-12-22 08:43:09 +0100

    Regenerate definition files and dist

--- d.ts/cookies.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Cookie {
-    [key: string]: string;
-    value: string;
-}
-declare const _default: {
-    parse: (str: string, options?: any) => Cookie;
-};
-export default _default;
