4ffcedc898ab977551db0210926b046903a33eb8
Sebastien Renault <srenault.contact@gmail.com>
2020-03-20 10:53:28 +0100

    Add support for PreviewResolver to client

---
+++ d.ts/PreviewResolver.d.ts
@@ -0,0 +1,9 @@
+import { Document } from "./documents";
+import { RequestCallback } from './request';
+export declare type LinkResolver = (doc: any) => string;
+export interface PreviewResolver {
+    token: string;
+    documentId?: string;
+    resolve(linkResolver: LinkResolver, defaultUrl: string, cb?: RequestCallback<string>): Promise<string>;
+}
+export declare function createPreviewResolver(token: string, documentId?: string, getDocByID?: (documentId: string, maybeOptions?: object) => Promise<Document>): PreviewResolver;
