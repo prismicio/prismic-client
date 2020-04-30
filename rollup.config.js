2b9bdff735b80dd734f85c9f8ff817963dffb5a3
Vincent Velociter <vincent.velociter@gmail.com>
2020-04-30 17:48:35 +0200

    Provide minified alternatives

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,11 @@ export default {
   input: 'src/index.ts',
   output: [
     { file: `cjs/${pkg.name}.js`, format: 'cjs', sourcemap: true },
+    { file: `cjs/${pkg.name}.min.js`, format: 'cjs', sourcemap: true, plugins: [terser()] },
+
     { file: `esm/${pkg.name}.mjs`, format: 'esm', sourcemap: true },
+    { file: `esm/${pkg.name}.min.mjs`, format: 'esm', sourcemap: true, plugins: [terser()] },
+
     { file: `umd/${pkg.name}.js`, format: 'umd', name: 'PrismicJS', globals: { 'cross-fetch': 'fetch' }, sourcemap: true },
     { file: `umd/${pkg.name}.min.js`, format: 'umd', name: 'PrismicJS', globals: { 'cross-fetch': 'fetch' }, sourcemap: true, plugins: [terser()] },
   ],
