d6e71688c2840f34da736c39ab90f554cf0167e5
Vincent Velociter <vincent.velociter@gmail.com>
2020-08-19 10:56:10 +0200

    Remove types from git

--- types/Cookies.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Cookie {
-    [key: string]: string;
-    value: string;
-}
-declare function parse(str: string, options?: any): Cookie;
-declare const _default: {
-    parse: typeof parse;
-};
-export default _default;
