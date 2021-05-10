f8dcee9608015b285957fbf529e6829d470af663
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-05-10 11:47:00 -1000

    chore: add TODO to widen fetch type

--- test/__testutils__/createClient.ts
+++ test/__testutils__/createClient.ts
@@ -11,6 +11,9 @@ export const createTestClient = (
   const repositoryName = crypto.createHash('md5').update(t.title).digest('hex')
   const endpoint = `https://${repositoryName}.cdn.prismic.io/api/v2`
   const resolvedOptions: prismic.ClientConfig = {
+    // TODO: Need to figure out how to widen the type of fetch to ensure users
+    // can provide their own fetch functio nwithout type issues.
+    // @ts-expect-error - The types for Fetch and node-fetch do not match
     fetch,
     ...options,
   }
