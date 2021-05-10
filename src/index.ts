2bdfd3ff8f40897b7606f00846a5a75b55a1f7bd
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-05-10 11:46:36 -1000

    refactor: export all in root

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-export { getEndpoint } from './getEndpoint'
-export { createClient, Client } from './client'
-export { buildQueryURL } from './buildQueryURL'
-export * as cookie from './cookie'
-export * from './types'
+// Primary library API.
+export * from './getEndpoint'
+export * from './buildQueryURL'
+export * from './client'
 
-export type { ClientConfig } from './client'
-export type { QueryParams, BuildQueryURLArgs } from './buildQueryURL'
+// A collection of well-known cookies shared between Prismic libraries and systems.
+export * as cookie from './cookie'
+
+// General types used throughout the project. These are made public to allow users to better type their projects.
+export * from './types'
