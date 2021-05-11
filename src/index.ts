18d742c4e6476a22e23b9d860a50734b303c23a6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-05-10 14:57:23 -1000

    feat: export predicates under root package

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ export * from './getEndpoint'
 export * from './buildQueryURL'
 export * from './client'
 
-// A collection of well-known cookies shared between Prismic libraries and systems.
+// Predicates API.
+export * as predicate from './predicate'
+
+// A collection of well-known cookie names shared between Prismic libraries and systems.
 export * as cookie from './cookie'
 
 // General types used throughout the project. These are made public to allow users to better type their projects.
