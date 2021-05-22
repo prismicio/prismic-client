b04d35934c42b752776c22f75ecc001148369053
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-05-21 14:21:55 -1000

    docs: add comment to server-usage example

--- examples/server-usage/index.js
+++ examples/server-usage/index.js
@@ -3,6 +3,9 @@ import got from 'got'
 
 const endpoint = prismic.getEndpoint('my-repo')
 const client = prismic.createClient(endpoint, {
+  // Here, we provide a way for the client to make network requests.
+  // Got is one way to make network requests on the server. It has built-in
+  // caching and automatically retries requests on failures.
   fetch: async (url, options) => {
     return await got(url, options).json()
   },
