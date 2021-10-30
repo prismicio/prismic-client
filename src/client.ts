7a5140e7be1fca2c1ddf1ad34b14e16604c491c6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-10-29 15:45:30 -1000

    fix: revert to providing access token via URL parameter

--- src/client.ts
+++ src/client.ts
@@ -949,7 +949,16 @@ export class Client {
 	 * @returns Repository metadata.
 	 */
 	async getRepository(): Promise<prismicT.Repository> {
-		return await this.fetch<prismicT.Repository>(this.endpoint);
+		// TODO: Restore when Authorization header support works in browsers with CORS.
+		// return await this.fetch<prismicT.Repository>(this.endpoint);
+
+		const url = new URL(this.endpoint);
+
+		if (this.accessToken) {
+			url.searchParams.set("access_token", this.accessToken);
+		}
+
+		return await this.fetch<prismicT.Repository>(url.toString());
 	}
 
 	/**
@@ -1082,7 +1091,7 @@ export class Client {
 			ref,
 			integrationFieldsRef,
 			routes: params.routes || this.routes,
-			accessToken: undefined,
+			accessToken: params.accessToken || this.accessToken,
 		});
 	}
 
@@ -1332,14 +1341,16 @@ export class Client {
 	 */
 	private async fetch<T = unknown>(
 		url: string,
-		params?: Partial<BuildQueryURLArgs>,
+		// TODO: Change to `params` when Authorization header support works in browsers with CORS.
+		_params?: Partial<BuildQueryURLArgs>,
 	): Promise<T> {
-		const accessToken = (params && params.accessToken) || this.accessToken;
-		const options = accessToken
-			? { headers: { Authorization: `Token ${accessToken}` } }
-			: {};
+		// TODO: Restore when Authorization header support works in browsers with CORS.
+		// const accessToken = (params && params.accessToken) || this.accessToken;
+		// const options = accessToken
+		// 	? { headers: { Authorization: `Token ${accessToken}` } }
+		// 	: {};
 
-		const res = await this.fetchFn(url, options);
+		const res = await this.fetchFn(url);
 
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		let json: any;
--- test/__testutils__/isValidAccessToken.ts
+++ test/__testutils__/isValidAccessToken.ts
@@ -1,13 +1,19 @@
 import * as msw from "msw";
 
-import { createAuthorizationHeader } from "./createAuthorizationHeader";
+// TODO: Restore when Authorization header support works in browsers with CORS.
+// import { createAuthorizationHeader } from "./createAuthorizationHeader";
 
 export const isValidAccessToken = (
 	accessToken: string | undefined,
 	req: msw.RestRequest,
 ): boolean => {
+	// TODO: Restore when Authorization header support works in browsers with CORS.
+	// return typeof accessToken === "string"
+	// 	? req.headers.get("Authorization") ===
+	// 			createAuthorizationHeader(accessToken)
+	// 	: true;
+
 	return typeof accessToken === "string"
-		? req.headers.get("Authorization") ===
-				createAuthorizationHeader(accessToken)
+		? req.url.searchParams.get("access_token") === accessToken
 		: true;
 };
