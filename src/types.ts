c3bd66c15d7580ec0326737ed8538e2d04d8117e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-05-25 14:25:24 -1000

    chore(deps): upgrade dependencies

--- src/types.ts
+++ src/types.ts
@@ -43,6 +43,10 @@ export type AbortSignalLike = {
 	dispatchEvent: any;
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	onabort: any;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	reason: any;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	throwIfAborted: any;
 };
 
 /**
