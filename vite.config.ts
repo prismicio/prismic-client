9e694d6bae3c497f00c6f48416ba0c27c9e6f9fd
lihbr <lihbr@users.noreply.github.com>
2022-10-04 11:55:53 +0200

    build: migrate to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import sdk from "vite-plugin-sdk";
 
 export default defineConfig({
+	plugins: [sdk()],
 	test: {
 		coverage: {
-			provider: "c8",
 			reporter: ["lcovonly", "text"],
 		},
 		setupFiles: ["./test/__setup__"],
