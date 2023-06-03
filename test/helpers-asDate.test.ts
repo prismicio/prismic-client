7cce22a35cd3ef327920cf8a8f8289d5c38cb7bc
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-06-02 16:13:56 -1000

    fix: type `PrismicDocument.*_publication_date` as `TimestampField<"filled">` (#304)
    * fix: type `PrismicDocument.*_publication_date` as `TimestampField<"filled">`
    
    * chore: remove self-referrential `@prismicio/client` dependency
    
    * refactor: remove `@ts-expect-error` comments

--- test/helpers-asDate.test.ts
+++ test/helpers-asDate.test.ts
@@ -1,5 +1,7 @@
 import { expect, it } from "vitest";
 
+import { documentFixture } from "./__fixtures__/document";
+
 import { asDate } from "../src";
 
 it("returns null for nullish inputs", () => {
@@ -29,4 +31,17 @@ it("returns a date object from a timestamp field", () => {
 	const field = "2021-05-11T22:00:00+0000";
 
 	expect(asDate(field)).toBeInstanceOf(Date);
+});
+
+it("is compatible with a document's first_publication_date and last_publication_date properties", () => {
+	// This test is functionally no different than the tests above, but we
+	// can at least check that the types between `PrismicDocument` and
+	// `asDate()` are compatible.
+
+	expect(asDate(documentFixture.empty.first_publication_date)).toBeInstanceOf(
+		Date,
+	);
+	expect(asDate(documentFixture.empty.last_publication_date)).toBeInstanceOf(
+		Date,
+	);
 });
