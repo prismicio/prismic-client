cdbedd79bfa31e0e5f5aff249f1e5ca0403af57e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-10-02 15:46:39 -1000

    fix: extend `RefNotFoundError` and `RefExpiredError` from `ForbiddenError` for backwards compatibility

--- src/errors/RefNotFoundError.ts
+++ src/errors/RefNotFoundError.ts
@@ -1,8 +1,14 @@
-import { PrismicError } from "./PrismicError";
+import { ForbiddenError } from "./ForbiddenError";
 
 type RefNotFoundErrorAPIResponse = {
 	type: "api_notfound_error";
 	message: string;
 };
 
-export class RefNotFoundError extends PrismicError<RefNotFoundErrorAPIResponse> {}
+// This error extends `ForbiddenError` for backwards compatibility. Before the
+// API started returning 404 for not found refs, it returnd 403, which threw a
+// `ForbiddenError`.
+// TODO: Extend this error from `PrismicError` in v8.
+export class RefNotFoundError<
+	TResponse = RefNotFoundErrorAPIResponse,
+> extends ForbiddenError<TResponse> {}
