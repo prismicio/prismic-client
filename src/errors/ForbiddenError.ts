26d5b0f022c61ed8711d99edce572bdbe013d3c9
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-10-05 14:13:33 -1000

    feat: support expired and not-found ref API errors (#327)
    * feat: support expired and not-found ref API errors
    
    * fix: extend `RefNotFoundError` and `RefExpiredError` from `ForbiddenError` for backwards compatibility

--- src/errors/ForbiddenError.ts
+++ src/errors/ForbiddenError.ts
@@ -9,6 +9,8 @@ type ForbiddenErrorQueryAPIResponse = {
 	error: string;
 };
 
-export class ForbiddenError extends PrismicError<
-	ForbiddenErrorRepositoryAPIResponse | ForbiddenErrorQueryAPIResponse
-> {}
+export class ForbiddenError<
+	TResponse =
+		| ForbiddenErrorRepositoryAPIResponse
+		| ForbiddenErrorQueryAPIResponse,
+> extends PrismicError<TResponse> {}
