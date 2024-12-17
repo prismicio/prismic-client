da023890fab663d41446bdeef640a5e1065d39c6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2024-12-17 12:58:00 -1000

    Revert "fix: add optional link fields to link to media migration fields"
    This reverts commit 6f89eeeeaf3b0f01e6c1ed2d0e3f5c60627445a4.

--- src/lib/isMigrationValue.ts
+++ src/lib/isMigrationValue.ts
@@ -85,10 +85,6 @@ export const image = (value: UnknownValue): value is MigrationImage => {
 /**
  * Checks if a value is a migration link to media field.
  *
- * @remarks
- * `OptionalLinkProperties` is included because `MigrationContentRelationship`
- * may be a link field, not strictly a content relationship field.
- *
  * @param value - Value to check.
  *
  * @returns `true` if `value` is a migration link to media field, `false`
@@ -99,7 +95,7 @@ export const image = (value: UnknownValue): value is MigrationImage => {
  */
 export const linkToMedia = (
 	value: UnknownValue,
-): value is MigrationLinkToMedia & OptionalLinkProperties => {
+): value is MigrationLinkToMedia => {
 	return (
 		typeof value === "object" &&
 		value !== null &&
