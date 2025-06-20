8a26cef3dc94884f9ea066eb738fed3c702d12a0
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-06-20 18:09:51 +0200

    feat: support content relationship v2

--- src/types/model/contentRelationship.ts
+++ src/types/model/contentRelationship.ts
@@ -2,13 +2,38 @@ import type { CustomTypeModelFieldType } from "./types"
 
 import type { CustomTypeModelLinkSelectType } from "./link"
 
+type GroupLevel2 = {
+	id: string
+	fields: ReadonlyArray<string>
+}
+
+type CustomTypeLevel2 = {
+	id: string
+	fields: ReadonlyArray<string | GroupLevel2>
+}
+
+type GroupLevel1 = {
+	id: string
+	fields: ReadonlyArray<string | ContentRelationshipLevel1>
+}
+
+type ContentRelationshipLevel1 = {
+	id: string
+	customtypes: ReadonlyArray<string | CustomTypeLevel2>
+}
+
+type CustomTypeLevel1 = {
+	id: string
+	fields: ReadonlyArray<string | GroupLevel1 | ContentRelationshipLevel1>
+}
+
 /**
  * A content relationship custom type field.
  *
  * More details: {@link https://prismic.io/docs/content-relationship}
  */
 export interface CustomTypeModelContentRelationshipField<
-	CustomTypeIDs extends string = string,
+	CustomTypeIDs extends string | CustomTypeLevel1 = string | CustomTypeLevel1,
 	Tags extends string = string,
 > {
 	type: typeof CustomTypeModelFieldType.Link
